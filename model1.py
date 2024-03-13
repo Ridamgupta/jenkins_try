ff4aa5fddb2a317c9ef454a3a19625d2ebdb00b7
Ridam Gupta <104208414+Ridamgupta@users.noreply.github.com>
2024-03-13 09:55:45 +0530

    Update model1.py

--- model1.py
+++ model1.py
@@ -1,6 +1,12 @@
 import pandas as pd
 import dtale
-import seaborn as sns
+import webbrowser
+
 df = pd.read_csv("loan.csv")
 d = dtale.show(df)
-d.open_browser()
+
+# Get the URL of the D-Tale dashboard
+url = d._url
+print(url)
+# Open the URL in the default web browser
+webbrowser.open_new_tab(url)
